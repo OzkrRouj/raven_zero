7b801beab26b39e8a2374f5e7e1f1e78bb3876ca
Oscar Rojas <ozkrrr9@gmail.com>
2025-12-17 15:28:22 -0500

    Update configuration file

--- app/config.py
+++ app/config.py
@@ -4,27 +4,46 @@ from pydantic import Field, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Setings(BaseSettings):
+class Settings(BaseSettings):
     """
     Everything that changes or defines the environment,
     you declare and validate it here;
     the code only executes the logic.
     """
 
-    redis_url: str = Field(description="Redis URL", default="redis://localhost:6379/0")
+    # --- INFRASTRUCTURE ---
+    redis_url: str = Field(
+        default="redis://localhost:6379/0", description="Redis connection string"
+    )
 
     @field_validator("redis_url")
     @classmethod
     def validate_url(cls, v: str) -> str:
         if not v.startswith("redis://"):
-            raise ValueError(f"redis_url must start with 'redis://'url used: {v}")
+            raise ValueError(f"redis_url must start with 'redis://'. URL used: {v}")
+        return v
 
-    storage_path: str = Field(
-        description="Directory for store uplads", default="./storage/uplad"
+    # --- FILE ROUTES ---
+    storage_path: Path = Field(
+        default=Path("./storage/uploads"), description="Directory for storing uploads"
     )
 
-    diceware_wordlist_path: Path = Path("data/diceware_words.txt")
+    diceware_wordlist_path: Path = Field(
+        default=Path("data/diceware_words.txt"),
+        description="Path to the diceware dictionary file",
+    )
 
+    @field_validator("diceware_wordlist_path")
+    @classmethod
+    def check_file_exists(cls, v: Path) -> Path:
+        if not v.exists():
+            raise ValueError(f"Wordlist file not found at: {v}")
+        return v
+
+    # --- CONFIGURATION ---
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", case_sensitive=False, extra="ignore"
     )
+
+
+settings = Settings()
